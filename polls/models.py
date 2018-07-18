68a003a1f70be759d1ae981a1e9a087c7f6ee012
venthent <13533801264@163.com>
2018-07-18 20:14:17 +0800

    all

--- polls/models.py
+++ polls/models.py
@@ -2,25 +2,24 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 # Create your models here.
 class Question(models.Model):
-    question_text=models.CharField(max_length=200)
-    pub_date=models.DateTimeField('date published')  #定义了对人类友好的名字
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')  # 定义了对人类友好的名字
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        now=timezone.now()
-        return timezone.now()-datetime.timedelta(days=1)<=self.pub_date<=now
+        now = timezone.now()
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
-    question=models.ForeignKey(Question,on_delete=models.CASCADE)
-    choice_text=models.CharField(max_length=200)
-    votes=models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
-
-
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wangjie/PycharmProjects/LearningDjango/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
